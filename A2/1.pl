c30070f2c876e01ba4e1ecaad2362018cc1b4617
Saurabh-Bazari <saurabhbazari3101@gmail.com>
2019-10-22 17:55:06 +0530

    complete A2 q1

--- A2/1.pl
+++ A2/1.pl
@@ -1,22 +1,41 @@
 decode_recursive([],1,0).
+
+decode_recursive([H|T],Ans,AnsPerv) :- 
+                                not(atom_number(H,Hnumber)),
+                                write("There is not only Integer"),nl,
+                                Ans is 0,
+                                AnsPerv is 0.
+
+decode_recursive([H|T],Ans,AnsPerv) :- 
+                                length(T,N),
+                                N > 0,
+                                atom_number(H,Hnumber),
+                                [SubH|SubT] = T,
+                                not(atom_number(SubH,SubHnumber)),
+                                write("There is not only Integer"),nl,
+                                Ans = 0,
+                                AnsPerv = 0.
+
 decode_recursive([H|T],Ans,AnsPerv) :- 
                                 length(T,N),
                                 N==0,
                                 atom_number(H,Hnumber),
                                 Hnumber==0,
-                                write("type 1"),nl,
+                                write("type2"),nl,
                                 decode_recursive(T,AnsForT,AnsPervForT),
                                 Ans is 0,
                                 AnsPerv is AnsForT.
+
 decode_recursive([H|T],Ans,AnsPerv) :- 
                                 length(T,N),
                                 N==0,
                                 atom_number(H,Hnumber),
                                 Hnumber > 0,
-                                write("type 2"),nl,
+                                write("type3"),nl,
                                 decode_recursive(T,AnsForT,AnsPervForT),
                                 Ans is 1,
                                 AnsPerv is AnsForT.
+
 decode_recursive([H|T],Ans,AnsPerv) :- 
                                 length(T,N),
                                 N > 0,
@@ -24,10 +43,11 @@ decode_recursive([H|T],Ans,AnsPerv) :-
                                 [SubH|SubT] = T,
                                 atom_number(SubH,SubHnumber),
                                 Hnumber == 0,
-                                write("tyListpe 3"),nl,
+                                write("type4"),nl,
                                 decode_recursive(T,AnsForT,AnsPervForT),
                                 Ans = 0,
                                 AnsPerv = AnsForT.
+
 decode_recursive([H|T],Ans,AnsPerv) :- 
                                 length(T,N),
                                 N > 0,
@@ -35,10 +55,11 @@ decode_recursive([H|T],Ans,AnsPerv) :-
                                 [SubH|SubT] = T,
                                 atom_number(SubH,SubHnumber),
                                 Hnumber == 1,
-                                write("type 4"),nl,
+                                write("type5"),nl,
                                 decode_recursive(T,AnsForT,AnsPervForT),
                                 Ans is AnsForT + AnsPervForT,
                                 AnsPerv = AnsForT.
+
 decode_recursive([H|T],Ans,AnsPerv) :- 
                                 length(T,N),
                                 N > 0,
@@ -47,10 +68,11 @@ decode_recursive([H|T],Ans,AnsPerv) :-
                                 atom_number(SubH,SubHnumber),
                                 Hnumber == 2,
                                 SubHnumber <7,
-                                write("type 5"),nl,
+                                write("type6"),nl,
                                 decode_recursive(T,AnsForT,AnsPervForT),
                                 Ans is AnsForT + AnsPervForT,
                                 AnsPerv = AnsForT.
+
 decode_recursive([H|T],Ans,AnsPerv) :- 
                                 length(T,N),
                                 N > 0,
@@ -59,10 +81,11 @@ decode_recursive([H|T],Ans,AnsPerv) :-
                                 atom_number(SubH,SubHnumber),
                                 Hnumber == 2,
                                 SubHnumber > 6,
-                                write("type 6"),nl,
+                                write("type7"),nl,
                                 decode_recursive(T,AnsForT,AnsPervForT),
                                 Ans is AnsForT,
                                 AnsPerv = AnsForT.
+
 decode_recursive([H|T],Ans,AnsPerv) :- 
                                 length(T,N),
                                 N > 0,
@@ -70,7 +93,7 @@ decode_recursive([H|T],Ans,AnsPerv) :-
                                 [SubH|SubT] = T,
                                 atom_number(SubH,SubHnumber),
                                 Hnumber > 2,
-                                write("type 7"),nl,
+                                write("type8"),nl,
                                 decode_recursive(T,AnsForT,AnsPervForT),
                                 Ans is AnsForT,
                                 AnsPerv = AnsForT.
